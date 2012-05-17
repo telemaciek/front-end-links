6b609e54684efc69f9d6d5a0d8f1e9577d47c0f3
telemaciek <szafraniec@gmail.com>
2012-05-17 21:11:44 +0200

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 FrontEndLinks::Application.routes.draw do
+  get "home/index"
+
+  get "home/about"
+
   resources :links
 
   resources :categories
+
+  match 'about' => 'home#about', :as => :about
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +58,7 @@ FrontEndLinks::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
